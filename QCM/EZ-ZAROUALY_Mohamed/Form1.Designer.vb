852097c65ce940e3ec3d7771908dde814ea811ab
Mohamed <soufian@monadem.com>
2022-08-03 11:51:07 +0100

    The whole project

---
+++ QCM/EZ-ZAROUALY_Mohamed/Form1.Designer.vb
@@ -0,0 +1,380 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class QCM
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(39, 58)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(721, 141)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Informations"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(384, 33)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(151, 20)
+        Me.TextBox2.TabIndex = 16
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(93, 33)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(151, 20)
+        Me.TextBox1.TabIndex = 15
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(165, 109)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(160, 21)
+        Me.ComboBox1.TabIndex = 14
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(359, 103)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(89, 31)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Lancer QCM"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(611, 27)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(89, 31)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "S'identifier" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(67, 112)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(86, 13)
+        Me.Label10.TabIndex = 11
+        Me.Label10.Text = "Numéro du QCM"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(643, 71)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(0, 13)
+        Me.Label9.TabIndex = 10
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(527, 71)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(99, 13)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "Date de naissance:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(401, 71)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(0, 13)
+        Me.Label7.TabIndex = 8
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(291, 71)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(46, 13)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Prénom:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(172, 71)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 13)
+        Me.Label5.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(29, 71)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(32, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Nom:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(268, 36)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(74, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Mot de passe:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 36)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Matricule:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Controls.Add(Me.RadioButton3)
+        Me.GroupBox2.Controls.Add(Me.RadioButton2)
+        Me.GroupBox2.Controls.Add(Me.RadioButton1)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.Button4)
+        Me.GroupBox2.Controls.Add(Me.Button5)
+        Me.GroupBox2.Controls.Add(Me.Button6)
+        Me.GroupBox2.Controls.Add(Me.Label13)
+        Me.GroupBox2.Controls.Add(Me.Label12)
+        Me.GroupBox2.Controls.Add(Me.Label11)
+        Me.GroupBox2.Location = New System.Drawing.Point(42, 236)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(717, 257)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "QCM"
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(67, 155)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(90, 17)
+        Me.RadioButton4.TabIndex = 21
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "RadioButton4"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(67, 132)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(90, 17)
+        Me.RadioButton3.TabIndex = 20
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "RadioButton3"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(67, 109)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(90, 17)
+        Me.RadioButton2.TabIndex = 19
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "RadioButton2"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(67, 84)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(90, 17)
+        Me.RadioButton1.TabIndex = 18
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(443, 19)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(89, 26)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "Terminer"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(551, 19)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(89, 26)
+        Me.Button4.TabIndex = 15
+        Me.Button4.Text = "Quitter"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(113, 201)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(89, 31)
+        Me.Button5.TabIndex = 16
+        Me.Button5.Text = "Précédente"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(461, 201)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(89, 31)
+        Me.Button6.TabIndex = 17
+        Me.Button6.Text = "Suivante"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(327, 60)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(0, 13)
+        Me.Label13.TabIndex = 9
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(169, 60)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(0, 13)
+        Me.Label12.TabIndex = 8
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(101, 60)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(49, 13)
+        Me.Label11.TabIndex = 7
+        Me.Label11.Text = "Question"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(330, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(125, 25)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Participant"
+        '
+        'QCM
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 505)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "QCM"
+        Me.Text = "QCM"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+End Class
---
+++ QCM/EZ-ZAROUALY_Mohamed/Form1.vb
@@ -0,0 +1,222 @@
+﻿Imports System.Data.OleDb
+Public Class QCM
+    Dim strConnection As String = "Provider =Microsoft.Jet.OLEDB.4.0; Data Source=..\..\AppData\QCM.mdb"
+    Dim oConnection As New OleDbConnection(strConnection)
+    Dim strRequest As String
+    Dim compteur As Integer = 0
+    Dim reponsesvides As Integer = 0
+
+    Function correction()
+        Dim a As Integer
+        strRequest = "SELECT count (Select NumeroQuestion from Notes where MatriculeStudent= " & CInt(TextBox1.Text) & ")  from Notes ;"
+        Try
+            Dim oCommand As New OleDbCommand(strRequest, oConnection)
+            oConnection.Open()
+            Dim oreader As OleDbDataReader = oCommand.ExecuteReader()
+            a = oreader.GetValue(0)
+            oConnection.Close()
+        Catch ex As Exception
+            MsgBox("Erreur :" + ex.Message)
+        End Try
+        Return a
+    End Function
+
+    Private Sub QCM_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button4.Enabled = True
+        Button1.Enabled = True
+        Button2.Enabled = False
+        Button3.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+    End Sub
+    Sub vides(y As Integer)
+        If RadioButton1.Checked = False And RadioButton2.Checked = False And RadioButton3.Checked = False And RadioButton4.Checked = False Then
+            y = y + 1
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        strRequest = "select Matricule from Students;"
+        Dim a As Integer = 0
+        Try
+            Dim oCommand As New OleDbCommand(strRequest, oConnection)
+            oConnection.Open()
+            Dim oreader As OleDbDataReader = oCommand.ExecuteReader()
+            While oreader.Read
+                If oreader.GetValue(0) = Val(TextBox1.Text) Then
+                    a = 1
+                End If
+            End While
+            oConnection.Close()
+        Catch ex As Exception
+            MsgBox("Erreur :" + ex.Message)
+        End Try
+        strRequest = "select MotPasse from Students;"
+        Dim b As Integer = 0
+        Try
+            Dim oCommand As New OleDbCommand(strRequest, oConnection)
+            oConnection.Open()
+            Dim oreader As OleDbDataReader = oCommand.ExecuteReader()
+            While oreader.Read
+                If oreader.GetValue(0) = CStr(TextBox2.Text) Then
+                    b = 1
+                End If
+            End While
+            oConnection.Close()
+        Catch ex As Exception
+            MsgBox("Erreur :" + ex.Message)
+        End Try
+        If a = 1 And b = 1 Then
+
+            strRequest = "select Nom,Prenom,DateNaissance from Students where Matricule=" & Val(TextBox1.Text) & " ;"
+            Try
+                Dim oCommand As New OleDbCommand(strRequest, oConnection)
+                oConnection.Open()
+                Dim oreader As OleDbDataReader = oCommand.ExecuteReader()
+                While oreader.Read
+                    Label5.Text = oreader.GetString(0)
+                    Label7.Text = oreader.GetString(1)
+                    Label9.Text = oreader.GetValue(2)
+
+                End While
+                oConnection.Close()
+            Catch ex As Exception
+                MsgBox("Erreur :" + ex.Message)
+            End Try
+        Else
+            MsgBox("Les informations que vous avez rentré sont erronés ")
+            Me.Close()
+
+        End If
+        strRequest = "select distinct NumeroQCM from Questions;"
+        Try
+            Dim oCommand As New OleDbCommand(strRequest, oConnection)
+            oConnection.Open()
+            Dim oreader As OleDbDataReader = oCommand.ExecuteReader()
+            While oreader.Read
+                ComboBox1.Items.Add(oreader.GetValue(0))
+            End While
+            oConnection.Close()
+        Catch ex As Exception
+            MsgBox("Erreur :" + ex.Message)
+        End Try
+        Button1.Enabled = False
+        Button2.Enabled = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Button6.Enabled = True
+        Button2.Enabled = False
+        strRequest = "select Question,Reponse1,Reponse2,Reponse3,Reponse4 from Questions where NumeroQCM=" & CInt(ComboBox1.SelectedItem) & " ;"
+        Dim i As Integer
+        Try
+            Dim oCommand As New OleDbCommand(strRequest, oConnection)
+            oConnection.Open()
+            Dim oreader As OleDbDataReader = oCommand.ExecuteReader()
+            While oreader.Read
+                If compteur = i Then
+                    Label13.Text = oreader.GetString(0)
+                    Label12.Text = CStr(compteur + 1)
+                    RadioButton1.Text = oreader.GetString(1)
+                    RadioButton2.Text = oreader.GetString(2)
+                    RadioButton3.Text = oreader.GetString(3)
+                    RadioButton4.Text = oreader.GetString(4)
+                End If
+                i = i + 1
+            End While
+            oConnection.Close()
+        Catch ex As Exception
+            MsgBox("Erreur :" + ex.Message)
+        End Try
+        Button3.Enabled = True
+        Button4.Enabled = False
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Button5.Enabled = False
+        vides(reponsesvides)
+        If MsgBox("Vous voulez vraiment terminer le QCM ? ", MsgBoxStyle.YesNo, vbQuestion) = vbYes Then
+            MsgBox("Les réponses vides sont = " & reponsesvides)
+            Button3.Enabled = False
+            Button4.Enabled = True
+
+
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        RadioButton3.Checked = False
+        RadioButton4.Checked = False
+        compteur = compteur + 1
+        Button5.Enabled = True
+        strRequest = "select Question,Reponse1,Reponse2,Reponse3,Reponse4 from Questions where NumeroQCM=" & Val(ComboBox1.SelectedItem) & " ;"
+        Dim i As Integer = 0
+        Try
+            Dim oCommand As New OleDbCommand(strRequest, oConnection)
+            oConnection.Open()
+            Dim oreader As OleDbDataReader = oCommand.ExecuteReader()
+            While oreader.Read
+                If compteur = i Then
+                    Label13.Text = oreader.GetString(0)
+                    Label12.Text = CStr(compteur + 1)
+                    RadioButton1.Text = oreader.GetString(1)
+                    RadioButton2.Text = oreader.GetString(2)
+                    RadioButton3.Text = oreader.GetString(3)
+                    RadioButton4.Text = oreader.GetString(4)
+                End If
+                i = i + 1
+            End While
+
+            oConnection.Close()
+        Catch ex As Exception
+            MsgBox("Erreur :" + ex.Message)
+        End Try
+        If compteur = 3 Then
+            Button6.Enabled = False
+        End If
+        vides(reponsesvides)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        RadioButton3.Checked = False
+        RadioButton4.Checked = False
+        compteur = compteur - 1
+        Button6.Enabled = True
+        strRequest = "select Question,Reponse1,Reponse2,Reponse3,Reponse4 from Questions where NumeroQCM=" & Val(ComboBox1.SelectedItem) & " ;"
+        Dim i As Integer = 0
+        Try
+            Dim oCommand As New OleDbCommand(strRequest, oConnection)
+            oConnection.Open()
+            Dim oreader As OleDbDataReader = oCommand.ExecuteReader()
+            While oreader.Read
+                If compteur = i Then
+                    Label13.Text = oreader.GetString(0)
+                    Label12.Text = CStr(compteur + 1)
+                    RadioButton1.Text = oreader.GetString(1)
+                    RadioButton2.Text = oreader.GetString(2)
+                    RadioButton3.Text = oreader.GetString(3)
+                    RadioButton4.Text = oreader.GetString(4)
+                End If
+                i = i + 1
+            End While
+
+            oConnection.Close()
+        Catch ex As Exception
+            MsgBox("Erreur :" + ex.Message)
+        End Try
+        If compteur = 0 Then
+            Button5.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If MsgBox("Vous êtes sûr de vouloir fermer l'application ?", vbOKCancel + vbQuestion) = vbOK Then
+            Me.Close()
+        End If
+    End Sub
+End Class
